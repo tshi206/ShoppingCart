9e86006762d00e78d1d0ed82929d6bcc1b1c5b90
tshi206 <tshi206@aucklanduni.ac.nz>
2018-05-20 00:40:41 +1200

    add master detail page

--- ShoppingCart/ShoppingCart/App.xaml.cs
+++ ShoppingCart/ShoppingCart/App.xaml.cs
@@ -8,7 +8,7 @@ namespace ShoppingCart
         {
             InitializeComponent();
 
-            MainPage = new ShoppingCartPage();
+            MainPage = new ShoppingCartMasterDetailPage();
         }
 
         protected override void OnStart()
---
+++ ShoppingCart/ShoppingCart/ShoppingCartMasterDetailPageDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShoppingCart
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShoppingCartMasterDetailPageDetail : ContentPage
+    {
+        public ShoppingCartMasterDetailPageDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
